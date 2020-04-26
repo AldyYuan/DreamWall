d0339c825041d64a412bb565685976284738fbe8
AldyYuan <yuanaldy@gmail.com>
2020-04-26 14:06:07 +0700

    Fix provider

--- lib/providers/pexels_provider.dart
+++ lib/providers/pexels_provider.dart
@@ -1,6 +1,7 @@
 import 'package:dream_wall/api/api.dart';
 import 'package:dream_wall/models/pages.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class PexelsProvider with ChangeNotifier {
   Future<Pages> getPhotos() async {
@@ -11,33 +12,42 @@ class PexelsProvider with ChangeNotifier {
     return null;
   }
 
+  Future<Pages> getOther() async {
+    var random = new Random();
+    final response =
+        await api.get("curated?per_page=15&page=${random.nextInt(1000)}");
+    if (response.statusCode == 200) {
+      return Pages.fromJson(response.data);
+    }
+    return null;
+  }
+
   Future<Pages> searchPages(String query) async {
     final response = await api.get("search?query=$query&per_page=80&page=1");
+
     if (response.statusCode == 200) {
-      if (response.statusCode == 200) {
-        return Pages.fromJson(response.data);
-      }
-      return null;
+      return Pages.fromJson(response.data);
     }
+    return null;
   }
 
-  Future<Pages> getPhotoById(String id) async {
+  Future<Pages> getPhotoById(double id) async {
     final response = await api.get("photos/$id");
+
     if (response.statusCode == 200) {
-      if (response.statusCode == 200) {
-        return Pages.fromJson(response.data);
-      }
-      return null;
+      return Pages.fromJson(response.data);
     }
+    return null;
   }
 
-  Future<Pages> getRandom({String page}) async {
-    final response = await api.get("curated?per_page=1&page=$page");
+  Future<Pages> getRandom() async {
+    var random = new Random();
+    final response =
+        await api.get("curated?per_page=1&page=${random.nextInt(1000)}");
+
     if (response.statusCode == 200) {
-      if (response.statusCode == 200) {
-        return Pages.fromJson(response.data);
-      }
-      return null;
+      return Pages.fromJson(response.data);
     }
+    return null;
   }
 }
