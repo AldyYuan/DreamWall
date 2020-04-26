46d86b1f8a0221691a12837a5be70ee3db202cbb
AldyYuan <yuanaldy@gmail.com>
2020-04-27 06:31:06 +0700

    Change Apps Name Icon & Splash

--- android/app/src/main/kotlin/com/example/dream_wall/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dream_wall/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.dream_wall
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
